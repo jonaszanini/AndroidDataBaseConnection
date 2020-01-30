3e8931943a91a402fd9817481f78f94b52ffb3b3
Jonas Zanini <jonaszanini@gmail.com>
2020-01-30 11:44:00 -0500

    Update and Delete added to the form

--- app/src/main/java/com/example/exercise2sqlitedatabasepart1/DataBaseHelper.java
+++ app/src/main/java/com/example/exercise2sqlitedatabasepart1/DataBaseHelper.java
@@ -59,4 +59,25 @@ public class DataBaseHelper extends SQLiteOpenHelper {
 
         return res;
     }
+
+    public boolean updateData(String id, String name, String surname, String marks){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_1, id);
+        contentValues.put(COL_2, name);
+        contentValues.put(COL_3, surname);
+        contentValues.put(COL_4, marks);
+        db.update(TABLE_NAME, contentValues, "ID = ?", new String[] { id });
+
+        return true;
+    }
+
+    public int deleteData(String id){
+
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        return db.delete(TABLE_NAME, "ID = ? ", new String[] {id});
+
+    }
+
 }
--- app/src/main/java/com/example/exercise2sqlitedatabasepart1/MainActivity.java
+++ app/src/main/java/com/example/exercise2sqlitedatabasepart1/MainActivity.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.net.Uri;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -17,8 +18,8 @@ import java.util.HashMap;
 public class MainActivity extends AppCompatActivity {
 
     DataBaseHelper myDB;
-    EditText editName, editSurname, editMarks;
-    Button btnAddData, btnViewAll;
+    EditText editName, editSurname, editMarks, editId;
+    Button btnAddData, btnViewAll, btnUpdate, btnDelete, btnCall, btnEmail, btnWebSite;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,10 +31,96 @@ public class MainActivity extends AppCompatActivity {
         editName = findViewById(R.id.editTextName);
         editSurname = findViewById(R.id.editTextSurname);
         editMarks = findViewById(R.id.editTextMarks);
+        editId = findViewById(R.id.editTextId);
         btnAddData = findViewById(R.id.button_Add);
         btnViewAll = findViewById(R.id.button_ViewAll);
+        btnUpdate = findViewById(R.id.button_Update);
+        btnDelete = findViewById(R.id.button_Delete);
+        btnCall = findViewById(R.id.button_Call);
+        btnEmail = findViewById(R.id.button_Gmail);
+        btnWebSite = findViewById(R.id.buttonWebSite);
         AddData();
         ViewAll();
+        UpdateData();
+        DeleteData();
+        MakeCall();
+        OpenEmailApp();
+        OpenWebSite();
+    }
+
+    public void MakeCall(){
+
+        btnCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:0987654321"));
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void OpenEmailApp() {
+        btnEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                Intent intent = getPackageManager().getLaunchIntentForPackage("com.google.android.gm");
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void OpenWebSite(){
+        btnWebSite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "http://www.jonaszanini.com";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
+    }
+
+    public void sendMail(){
+
+    }
+
+    public void DeleteData(){
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Integer isDeleted = myDB.deleteData(editId.getText().toString());
+
+                if (isDeleted > 0){
+                    Toast.makeText(MainActivity.this, "Data deleted", Toast.LENGTH_LONG).show();
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "Data NOT deleted", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+    }
+
+    public void UpdateData(){
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean isUpdated = myDB.updateData( editId.getText().toString(), editName.getText().toString(),
+                        editSurname.getText().toString(), editMarks.getText().toString());
+
+                if (isUpdated == true){
+                    Toast.makeText(MainActivity.this, "Data updated", Toast.LENGTH_LONG).show();
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "Data NOT updated", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
     public void ViewAll(){
@@ -58,10 +145,10 @@ public class MainActivity extends AppCompatActivity {
                     buffer.append("Marks: " + res.getString(3) + "\n\n");
                 }
 
-                //ShowMessage("Data", buffer.toString());
+                ShowMessage("Data", buffer.toString());
 
-                Intent intent = new Intent(MainActivity.this, ListStudents.class);
-                startActivity(intent);
+                //Intent intent = new Intent(MainActivity.this, ListStudents.class);
+                //startActivity(intent);
 
             }
         });
@@ -88,7 +175,6 @@ public class MainActivity extends AppCompatActivity {
                 }
                 else{
                     Toast.makeText(MainActivity.this, "Data NOT inserted", Toast.LENGTH_LONG).show();
-
                 }
             }
         });
